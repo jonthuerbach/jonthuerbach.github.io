a7d3e7c77fbe9b7257be93d480230e0d1aa047f6
Jon Thuerbach <jgthuerb@email.arizona.edu>
2015-03-02 15:24:14 -0700

    Add pace.js to site to try out

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
 	$(window).resize(function(){
 	  resizeCenter();
 	});
+	
 	function resizeCenter() {
     $('.hero-text').css({
         'position' : 'absolute',
@@ -27,6 +28,7 @@ $(document).ready(function(){
         'margin-left' : -$('.hero-text').outerWidth()/2,
         'margin-top' : -$('.hero-text').outerHeight()/2
     });
+    
     $('.hero-container').css({
         'width' : $( window ).outerWidth()
     });
@@ -34,11 +36,11 @@ $(document).ready(function(){
 
 	// BOOTSTRAP AFFIX
 	$('#main-nav').affix({
-  offset: {
-    top: function () {
-      return (this.top = $('.hero-zone').outerHeight(true)-$('.navbar').outerHeight(true))
-    }
-  }
-})
+	  offset: {
+	    top: function () {
+	      return (this.top = $('.hero-zone').outerHeight(true)-$('.navbar').outerHeight(true))
+	    }
+	  }
+	});
 
 })
