b87a9c77dc99c06a5b56ef723a0a83a5335da758
Jon <jgthuerbach@gmail.com>
2015-11-13 02:54:45 -0700

    Add scripts

--- sjdstyle/js/scripts.js
+++ sjdstyle/js/scripts.js
@@ -1,6 +1,33 @@
-$( document ).ready(function() {
-	// parallax
+$(document).ready(function() {
+	
+	// OFFSET FIXED MENU
+	$('body').css('padding-top', function() {
+    return $('#navbar-sjd').innerHeight();
+  });
+
+	// SET SCREEN HEIGHT TO HERO IMAGE
+	$('#hero').css('height', function() {
+    var screenHeight = $(window).height() - $('#navbar-sjd').innerHeight();
+    return screenHeight;
+  });
+
+	// PARALLAX
 	$(window).scroll(function () {
-	    $(".hero-block").css("background-position","0% " + ($(this).scrollTop() / 2) + "px");
+	    $("#hero").css("background-position","0% " + ($(this).scrollTop() / 2) + "px");
 	});
-});
+
+	// SMOOTH SCROLLING
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 850, 'swing');
+        return false;
+      }
+    }
+  });
+
+}); // END .ready()
