ee65204f1414f9538100386fbadff88a17d0261b
Jon <jgthuerbach@gmail.com>
2015-12-26 22:06:09 -0700

    Refactor game code to expand on food/wood/rock objects

--- games/rch/game.js
+++ games/rch/game.js
@@ -4,7 +4,7 @@ $(function() {
 });
 
 $( document ).ready(function() {
-  
+
   // Game Save Mechanics
   function loadGame() {
     var result = localStorage.getItem("save");
@@ -18,13 +18,13 @@ $( document ).ready(function() {
     if (r === true) {
       localStorage.removeItem("save");
       Game.isNewPlayer = true;
-      Game.food = 0;
-      Game.wood = 0;
-      Game.stone = 0;
-      Game.workers= 0;
-      Game.workersIncrement= 1;
-      Game.workersUnemployed= 0;
-      Game.workersFarmer= 0;
+      Game.food.current = 0;
+      Game.wood.current = 0;
+      Game.rock.current = 0;
+      Game.workers = 0;
+      Game.workersIncrement = 1;
+      Game.workersUnemployed = 0;
+      Game.workersFarmer = 0;
       refreshView();
     }
   };
@@ -47,22 +47,27 @@ $( document ).ready(function() {
       btnResetGame = $('#btn-reset-game'),
       btnGetFood = $('#btn-get-food'),
       btnGetWood = $('#btn-get-wood'),
-      btnGetStone = $('#btn-get-stone'),
+      btnGetRock = $('#btn-get-rock'),
       btnCreateWorker = $('#btn-create-worker'),
       
       // Display
       dispFood = $('#display-food'),
+      dispFoodTotal = $('#display-food-total'),
+      dispFoodDps = $('#display-food-dps'),
       dispWood = $('#display-wood'),
-      dispStone = $('#display-stone'),
+      dispWoodTotal = $('#display-wood-total'),
+      dispWoodDps = $('#display-wood-dps'),
+      dispRock = $('#display-rock'),
+      dispRockTotal = $('#display-rock-total'),
+      dispRockDps = $('#display-rock-dps'),
       dispWorkersUnemployed = $('#display-workers-unemployed');
 
   //Define Game Object
   var Game = {
     isNewPlayer: false,
-    food: 0,
-    foodInc: 1,
-    wood: 0,
-    stone: 0,
+    food: { current: 0, total: 250, dps: 0, inc: 1 },
+    wood: { current: 0, total: 250, dps: 0, inc: 1 },
+    rock: { current: 0, total: 250, dps: 0, inc: 1 },
     workers: 0,
     workersIncrement: 1,
     workersUnemployed: 0,
@@ -78,9 +83,15 @@ $( document ).ready(function() {
 
   // Game Refresh View
   function refreshView() {
-    dispFood.text(Game.food);
-    dispWood.text(Game.wood);
-    dispStone.text(Game.stone);
+    dispFood.text(Game.food.current);
+    dispFoodTotal.text(Game.food.total);
+    dispFoodDps.text(Game.food.dps);
+    dispWood.text(Game.wood.current);
+    dispWoodTotal.text(Game.wood.total);
+    dispWoodDps.text(Game.wood.dps);
+    dispRock.text(Game.rock.current);
+    dispRockTotal.text(Game.rock.total);
+    dispRockDps.text(Game.rock.dps);
     dispWorkersUnemployed.text(Game.workersUnemployed);
   };
 
@@ -93,29 +104,30 @@ $( document ).ready(function() {
     resetGame();
   });
   btnGetFood.click(function() {
-    Game.food++;
+    Game.food.current++;
     refreshView();
   });
   btnGetWood.click(function() {
-    Game.wood++;
+    Game.wood.current++;
     refreshView();
   });
-  btnGetStone.click(function() {
-    Game.stone++;
+  btnGetRock.click(function() {
+    Game.rock++;
     refreshView();
   });
   btnCreateWorker.click(function() {
-    if (Game.food >= 20) {
+    if (Game.food.current >= 20) {
       Game.workers++;
       Game.workersUnemployed++;
-      Game.food -= 20;
+      Game.food.current -= 20;
       refreshView();
     }
   });
 
-  // Button Disabled Checker
+  // Button Disabled and DPS Checker
   setInterval(function() {
-    btnCreateWorker.prop('disabled', Game.food <= 19);
+    btnCreateWorker.prop('disabled', Game.food.current <= 19);
+
   }, 100);
 
   // Init the Game
