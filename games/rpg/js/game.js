6749251a74b4844af5ed51c13d0909745194e843
Jon <jgthuerbach@gmail.com>
2016-02-04 23:00:14 -0700

    Update game.js to include doc ready function

--- games/rpg/js/game.js
+++ games/rpg/js/game.js
@@ -1,20 +1,23 @@
-// Firebase
-var ref = new Firebase("https://rpg-game-thingy.firebaseio.com/");
+jQuery(document).ready(function($) {
+  // Firebase
+  var ref = new Firebase("https://rpg-game-thingy.firebaseio.com/");
 
-// Global Variables
-var dUsername = $("#d-username"),
-    dProfileImage = $("#d-profile-image"),
-    btnLogin = $("#btn-login");
+  // Global Variables
+  var dUsername = $("#d-username"),
+      dProfileImage = $("#d-profile-image"),
+      btnLogin = $("#btn-login");
 
-// Buttons
-btnLogin.click(function() {
-  ref.authWithOAuthPopup("google", function(error, authData) {
-    if (error) {
-      console.log("Login Failed!", error);
-    } else {
-      console.log(authData.google.profileImageURL);
-      dUsername.text(authData.google.displayName);
-    }
+  // Buttons
+  btnLogin.click(function() {
+    ref.authWithOAuthPopup("google", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log(authData.google.profileImageURL);
+        dUsername.text(authData.google.displayName);
+      }
+    });
   });
-});
-dUsername.text(authData.google.displayName);
+  dUsername.text(authData.google.displayName);
+});  
+  
