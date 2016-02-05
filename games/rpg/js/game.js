02d609f6be214cc69ea44c61bb2b17a054202ab8
Jon <jgthuerbach@gmail.com>
2016-02-04 22:51:37 -0700

    Initial commit

---
+++ games/rpg/js/game.js
@@ -0,0 +1,20 @@
+// Firebase
+var ref = new Firebase("https://rpg-game-thingy.firebaseio.com/");
+
+// Global Variables
+var dUsername = $("#d-username"),
+    dProfileImage = $("#d-profile-image"),
+    btnLogin = $("#btn-login");
+
+// Buttons
+btnLogin.click(function() {
+  ref.authWithOAuthPopup("google", function(error, authData) {
+    if (error) {
+      console.log("Login Failed!", error);
+    } else {
+      console.log(authData.google.profileImageURL);
+      dUsername.text(authData.google.displayName);
+    }
+  });
+});
+dUsername.text(authData.google.displayName);
