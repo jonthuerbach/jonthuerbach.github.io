5de1cf7e881f33bef45ef6d070b76506aa48067f
Jon <jgthuerbach@gmail.com>
2016-02-08 22:50:24 -0700

    Initial FamilyApp commit

---
+++ angular/familyapp/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name familyAppApp
+ * @description
+ * # familyAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('familyAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ angular/familyapp/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name familyAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the familyAppApp
+ */
+angular.module('familyAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
